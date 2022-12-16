ba750f60cb71b60bfc7b434cba3b33626a414562
Zayon Nieves <zayon.nieves1@gmail.com>
2022-12-15 20:00:22 -0500

    refactored code with JavaScript module for seperation of concerns

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+// import module to use functions
+import { getUserInput, calculateFinalTemp } from "./modules/functions.js";
+
 
 // Step #1: get user input
-// create function for getting input
-let startingTemp;
-let startingUnit;
-let finalUnit;
+// calculate function
+function calculate() {
+
+    // get user input from elements and validate data
+    let input = getUserInput();
+
+    let finalTemp = calculateFinalTemp(input[0], input[1], input[2]);
+}
+
+
+
+// add eventListener to 'Convert'/Submit button
+let submitBtn = document.querySelector('input[value=Convert]');
+submitBtn.addEventListener('click', calculate);
